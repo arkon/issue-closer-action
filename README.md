67860e9b97553b49bd29f4660f70c476007b1b0c
arkon <eugcheung94@gmail.com>
2020-04-08 19:28:42 -0400

    Modify action to close issues based on regexes matching

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
-# GitHub action to automatically close issues/PRs
+# GitHub action to automatically close issues
 
-Automatically close issues or pull requests whose body text does not match the specified regular expression pattern.
-This is useful to enforce usage of issue/PR templates.
+_This is a modified version of https://github.com/roots/issue-closer-action_
+
+Automatically close issues whose title or body text matches the specified regular expression pattern.
 
 ## Installation
 
@@ -9,25 +10,25 @@ To configure the action simply add the following lines to your `.github/main.wor
 
 ```yml
 name: Autocloser
-on: [issues, pull_request]
+on: [issues]
 jobs:
   autoclose:
     runs-on: ubuntu-latest
     steps:
-    - name: Autoclose issues that did not follow issue template
-      uses: roots/issue-closer@v1.1
+    - name: Autoclose issues
+      uses: arkon/issue-closer-action@v1.0
       with:
         repo-token: ${{ secrets.GITHUB_TOKEN }}
         issue-close-message: "@${issue.user.login} this issue was automatically closed because it did not follow the issue template"
-        issue-pattern: ".*guidelines for Contributing.*"
+        issue-title-pattern: ".*Placeholder title.*"
+        issue-body-pattern: ".*Remove me.*"
 ```
 
 ## Configuration
 
-`issue-close-message` and `pr-close-message` are ES6-style template literals which will be evaluated with the issue or pull request
+`issue-close-message` is an ES6-style template literals which will be evaluated with the issue
 webhook payload in context. The example above uses `${issue.user.login}` to get the author of the issue.
 
 * `issue` webhook [payload example](https://developer.github.com/v3/activity/events/types/#webhook-payload-example-15)
-* `pull_request` webhook [payload example](https://developer.github.com/v3/activity/events/types/#webhook-payload-example-28)
 
-`issue-pattern` and `pr-pattern` are strings which are compiled to JavaScript `Regexp`s.
+`issue-title-pattern` and `issue-body-pattern` are strings which are compiled to JavaScript `Regexp`s.
--- src/main.ts
+++ src/main.ts
@@ -4,20 +4,19 @@ import * as github from '@actions/github';
 async function run() {
   try {
     const issueCloseMessage: string = core.getInput('issue-close-message');
-    const prCloseMessage: string = core.getInput('pr-close-message');
 
-    if (!issueCloseMessage && !prCloseMessage) {
+    if (!issueCloseMessage) {
       throw new Error(
-        'Action must have at least one of issue-close-message or pr-close-message set'
+        'Action must have at least one of issue-close-message set'
       );
     }
 
-    const issuePattern: string = core.getInput('issue-pattern');
-    const prPattern: string = core.getInput('pr-pattern');
+    const issueBodyPattern: string = core.getInput('issue-body-pattern');
+    const issueTitlePattern: string = core.getInput('issue-title-pattern');
 
-    if (!issuePattern && !prPattern) {
+    if (!issueBodyPattern && !issueBodyPattern) {
       throw new Error(
-        'Action must have at least one of issue-pattern or pr-pattern set'
+        'Action must have at least one of issue-body-pattern or issue-title-pattern set'
       );
     }
 
@@ -28,18 +27,8 @@ async function run() {
     const context = github.context;
     const payload = context.payload;
 
-    if (payload.action !== 'opened') {
-      core.debug('No issue or PR was opened, skipping');
-      return;
-    }
-
-    // Do nothing if its not a pr or issue
-    const isIssue: boolean = !!payload.issue;
-
-    if (!isIssue && !payload.pull_request) {
-      core.debug(
-        'The event that triggered this action was not a pull request or issue, skipping.'
-      );
+    // Do nothing if it's wasn't being opened or it's not an issue
+    if (payload.action !== 'opened' || !payload.issue) {
       return;
     }
 
@@ -48,85 +37,45 @@ async function run() {
     }
 
     const issue: {owner: string; repo: string; number: number} = context.issue;
-    const patternString: string = isIssue ? issuePattern : prPattern;
 
-    if (!patternString) {
-      core.debug('No pattern provided for this type of contribution');
+    const bodyMatches: boolean = check(issueBodyPattern, payload?.issue?.body);
+    const titleMatches: boolean = check(issueTitlePattern, payload?.issue?.title);
+
+    // Do nothing if no match
+    if (!bodyMatches && !titleMatches) {
       return;
     }
 
-    const pattern: RegExp = new RegExp(patternString);
-    const body: string | undefined = getBody(payload);
-
-    core.debug(`Matching against pattern ${pattern}`);
-    if (body && body.match(pattern)) {
-      core.debug('Body matched. Nothing more to do.');
-      return;
-    } else {
-      core.debug('Body did not match');
-    }
-
-    // Do nothing if no message set for this type of contribution
-    const closeMessage: string = isIssue ? issueCloseMessage : prCloseMessage;
-
-    if (!closeMessage) {
-      core.debug('No close message template provided for this type of contribution');
-      return;
-    }
-
-    core.debug('Creating message from template');
-    const message: string = evalTemplate(closeMessage, payload)
-    const issueType: string = isIssue ? 'issue' : 'pull request';
-
-    // Add a comment to the appropriate place
-    core.debug(`Adding message: ${message} to ${issueType} ${issue.number}`);
-    if (isIssue) {
-      await client.issues.createComment({
-        owner: issue.owner,
-        repo: issue.repo,
-        issue_number: issue.number,
-        body: message
-      });
-      core.debug('Closing issue');
-      await client.issues.update({
-        owner: issue.owner,
-        repo: issue.repo,
-        issue_number: issue.number,
-        state: 'closed'
-      });
-    } else {
-      await client.pulls.createReview({
-        owner: issue.owner,
-        repo: issue.repo,
-        pull_number: issue.number,
-        body: message,
-        event: 'COMMENT'
-      });
-      core.debug('Closing PR');
-      await client.pulls.update({
-        owner: issue.owner,
-        repo: issue.repo,
-        pull_number: issue.number,
-        state: 'closed'
-      });
-    }
+    // Comment and close
+    await client.issues.createComment({
+      owner: issue.owner,
+      repo: issue.repo,
+      issue_number: issue.number,
+      body: evalTemplate(issueCloseMessage, payload)
+    });
+    await client.issues.update({
+      owner: issue.owner,
+      repo: issue.repo,
+      issue_number: issue.number,
+      state: 'closed'
+    });
   } catch (error) {
     core.setFailed(error.message);
     return;
   }
 }
 
-function getBody(payload): string | undefined {
-  if (payload.issue && payload.issue.body) {
-    return payload.issue.body;
-  }
+function check(patternString: string, text: string | undefined): boolean {
+  const pattern: RegExp = new RegExp(patternString);
 
-  if (payload.pull_request && payload.pull_request.body) {
-    return payload.pull_request.body;
+  if (text?.match(pattern)) {
+    return true;
+  } else {
+    return false;
   }
 }
 
-function evalTemplate(template, params) {
+function evalTemplate(template: string, params: any) {
   return Function(...Object.keys(params), `return \`${template}\``)(...Object.values(params));
 }
 
